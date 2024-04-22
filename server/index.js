85559710ca4cad34236640ba45a9bc00f5d887c3
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2024-04-22 17:15:18 +0200

    création de la premiere route de l'api permettant de creer un compte

--- server/index.js
+++ server/index.js
@@ -1,12 +1,28 @@
 import express from "express";
 import { createServer } from "node:http";
+import { createAccount } from "./routes/createAccount.js";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
 const server = createServer(app);
 
+// Enable CORS
+app.options("*", cors());
+
+// Parse JSON bodies (as sent by API clients)
+app.use(bodyParser.json());
+
+// Accept requests from the frontend
+app.use(cors({ origin: "http://localhost:3000" }));
+
+// default route
 app.get("/", (req, res) => {
   res.send("<h1>Backend</h1>");
 });
+
+// routes of the api application
+app.use("/api", createAccount);
 
 server.listen(3001, () => {
   console.log("server running at http://localhost:3001");
