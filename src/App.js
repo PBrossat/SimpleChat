1f1dfc56231c62789ebf830bd23c01d985efeb10
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2024-04-22 17:16:04 +0200

    appel du composant creéation de compte

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import { CreateAccount } from "./components/CreateAccount";
 
 function App() {
   return (
     <div className="App">
-      <p>Hello World SimpleMessage App</p>
+      <CreateAccount />
     </div>
   );
 }
