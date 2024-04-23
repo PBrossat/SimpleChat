f7b757a5a60a86722c79ca539a20fe5c5393d073
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2024-04-23 16:47:03 +0200

    Refactor

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,19 +1,58 @@
 import sqlite3 from "sqlite3";
 
-const DB_PATH = "../DB_Simple_Chat.db";
+const DB_PATH =
+  "/Users/pierrickbrossat/Documents/ALL/Projet Perso/SimpleChat/server/DB_Simple_chat.db";
 
 export function addUsers(user) {
-  const db = new sqlite3.Database(DB_PATH);
-  console.log(db);
+  return new Promise((resolve, reject) => {
+    const db = new sqlite3.Database(DB_PATH);
 
-  const sql = `INSERT INTO Users (username, password, email, name, surname) VALUES (?, ?, ?, ?, ?)`;
-  const values = [user.username, user.password, user.email, "", ""];
+    const sql = `INSERT INTO Users (username, password, email, name, surname) VALUES (?, ?, ?, ?, ?)`;
+    const values = [user.username, user.password, user.email, "", ""];
 
-  db.run(sql, values, (error) => {
-    if (error) {
-      console.error("Error adding user to the database:", error);
-    }
+    db.run(sql, values, (error) => {
+      db.close(); // Close the database connection
+
+      if (error) {
+        console.error("Error adding user to the database:", error);
+        reject(error);
+      } else {
+        resolve(); // If there is no error, resolve the promise
+      }
+    });
   });
+}
 
-  db.close();
+export function getUser(username) {
+  return new Promise((resolve, reject) => {
+    const db = new sqlite3.Database(DB_PATH);
+    const sql = `SELECT * FROM Users WHERE username = ?`;
+    const values = [username];
+
+    const user = {
+      username: "",
+      email: "",
+      name: "",
+      surname: "",
+    };
+
+    // Verify if the user exists in the database
+    db.get(sql, values, (error, row) => {
+      if (error) {
+        console.error("Error getting user from the database:", error);
+        reject(error);
+      }
+
+      if (row) {
+        user.username = row.username;
+        user.email = row.email;
+        user.name = row.name;
+        user.surname = row.surname;
+      }
+
+      resolve(user);
+    });
+
+    db.close();
+  });
 }
