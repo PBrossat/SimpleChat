07acac7768d7a96aed74f3414b126753014d06f6
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2024-04-23 23:07:18 +0200

    Suppression du password dans la reponse
    Pour eviter que le front ai accès au password on el supprime du côté du serveur

--- server/routes/login.js
+++ server/routes/login.js
@@ -7,6 +7,7 @@ const router = Router();
 router.post("/login", async (req, res) => {
   const body = req.body;
 
+  //console.log("body:", body);
   // hash the password
   const username = body.username;
 
@@ -26,6 +27,7 @@ router.post("/login", async (req, res) => {
         // Compare the passwords
         bcrypt.compare(password, passwordInDb, (err, result) => {
           if (err) {
+            // internal error
             console.error("Error comparing passwords:", err);
             res
               .status(500)
@@ -33,7 +35,10 @@ router.post("/login", async (req, res) => {
                 "An error occurred while comparing the passwords. Please try again later"
               );
           } else if (result) {
-            res.status(200).json(user); // send the user information to the client
+            // If the passwords match, send the user information to the client
+
+            delete user.password; // don't send the password to the client
+            res.status(200).json(user);
           } else {
             res.status(401).send("Incorrect password");
           }
