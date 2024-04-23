563d6c3caab6ead9117ca4b0e8a03f8460c194de
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2024-04-23 16:46:20 +0200

    Little maj

--- server/routes/createAccount.js
+++ server/routes/createAccount.js
@@ -5,7 +5,7 @@ import { addUsers } from "../controllers/userController.js";
 const router = Router();
 
 router.post("/create-account", async (req, res) => {
-  console.log(req.body);
+  //console.log(req.body);
   const body = req.body;
 
   // hash the password
@@ -16,15 +16,20 @@ router.post("/create-account", async (req, res) => {
     res.status(500).send("Error hashing password");
   });
 
-  console.log("username : " + username);
-  console.log("email : " + email);
-  console.log("password : " + password);
+  // console.log("username : " + username);
+  // console.log("email : " + email);
+  // console.log("password : " + password);
 
   // add the user to the database
   const user = { username, email, password };
-  addUsers(user);
-
-  res.status(200); // 200 is the default status code
+  addUsers(user)
+    .then(() => {
+      res.status(200).send("Account created successfully");
+    })
+    .catch((error) => {
+      console.error("Error adding user to the database:", error);
+      res.status(500).send("Error adding user to the database");
+    });
 });
 
 export { router as createAccount };
