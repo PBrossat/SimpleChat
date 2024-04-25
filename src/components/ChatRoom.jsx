146ac0a01103d4b62ac1e74947d732f3e72da84a
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2024-04-25 19:07:00 +0200

    commentaires pour plus tard

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ResearchUser } from "./ResearchUser";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export function ChatRoom() {
+  // const location = useLocation();
+  // const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   const handleBeforeUnload = (event) => {
+  //     event.preventDefault();
+  //     event.returnValue = ""; // Requise pour Chrome
+  //     const confirmationMessage = "Êtes-vous sûr de vouloir vous déconnecter ?";
+  //     // Affiche une boîte de dialogue pour confirmer la déconnexion
+  //     event.returnValue = confirmationMessage;
+  //     return confirmationMessage;
+  //   };
+
+  //   window.addEventListener("beforeunload", handleBeforeUnload);
+
+  //   return () => {
+  //     window.removeEventListener("beforeunload", handleBeforeUnload);
+  //   };
+  // }, [location.pathname, navigate]);
+
   return (
     <div>
       <ResearchUser />
