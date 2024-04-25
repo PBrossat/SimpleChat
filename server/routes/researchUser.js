94e64bca6f0e77ed98d0be43f0371418fe5dfe4c
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2024-04-26 00:09:35 +0200

    Maj de la route pour l'api
    Ajout de la fonction permettant de verifier la validité d'un token et de renvoyer la playload du token en question (l'information du token).
    
    TODO : changer l'emplacement de la fonction dans un fichier utils.js

--- server/routes/researchUser.js
+++ server/routes/researchUser.js
@@ -1,11 +1,33 @@
+import "dotenv/config";
 import { Router } from "express";
 import { getAllUsersContainingInput } from "../controllers/userController.js";
+import jwt from "jsonwebtoken";
 
 const router = Router();
 
-router.get("/researchUser", async (req, res) => {
-  const usernameToSearch = req.query.username; // after the "username?" in the URL
+// Function to verify the token and return the decoded value of the token
+async function verifyToken(token) {
+  try {
+    const decoded = jwt.verify(token, process.env.ACCES_TOKEN_SECRET);
+    return decoded;
+  } catch (err) {
+    console.error("Error verifying the token:", err);
+    return { error: "Unauthorized: invalid token" };
+  }
+}
 
+router.get("/researchUser/:query", async (req, res) => {
+  // Check if the token is valid
+  // Authorization : Bearer <token>
+  const token = req.headers.authorization.split(" ")[1];
+  let user = null;
+  user = await verifyToken(token); // Verify and decode the token
+
+  if (user.error) {
+    return res.status(401).send(user.error);
+  }
+
+  const usernameToSearch = req.params.query;
   getAllUsersContainingInput(usernameToSearch)
     .then((users) => {
       res.status(200).json(users);
