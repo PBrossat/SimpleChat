e11e9544126e129f3fc5dc86203c7dd12c44eae9
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2024-04-26 17:24:04 +0200

    Ajout du nom de l'utilisateur courrent pour debbug

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -1,31 +1,21 @@
-import React, { useEffect } from "react";
-import { ResearchUser } from "./ResearchUser";
-import { useLocation, useNavigate } from "react-router-dom";
+import "./Discussions.jsx";
+import React, { useState } from "react";
+import { Discussions } from "./Discussions.jsx";
+import "../style/ChatRoom.css";
 
 export function ChatRoom() {
-  // const location = useLocation();
-  // const navigate = useNavigate();
-
-  // useEffect(() => {
-  //   const handleBeforeUnload = (event) => {
-  //     event.preventDefault();
-  //     event.returnValue = ""; // Requise pour Chrome
-  //     const confirmationMessage = "Êtes-vous sûr de vouloir vous déconnecter ?";
-  //     // Affiche une boîte de dialogue pour confirmer la déconnexion
-  //     event.returnValue = confirmationMessage;
-  //     return confirmationMessage;
-  //   };
-
-  //   window.addEventListener("beforeunload", handleBeforeUnload);
-
-  //   return () => {
-  //     window.removeEventListener("beforeunload", handleBeforeUnload);
-  //   };
-  // }, [location.pathname, navigate]);
+  const localStorage = window.localStorage;
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("user"))
+  );
 
   return (
-    <div>
-      <ResearchUser />
+    <div className="chat-room">
+      {/* barre latérale  */}
+      {/* liste des discussions */}
+      <Discussions />
+      {/* chat */}
+      Bonjour : {currentUser.username}
     </div>
   );
 }
