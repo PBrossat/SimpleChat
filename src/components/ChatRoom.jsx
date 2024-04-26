5c8cac1287a9f85c0d0e7e02ff96c92e91c14a54
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2024-04-27 01:20:24 +0200

    utilisation d'un objet au lieu d'un state

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -5,9 +5,7 @@ import "../style/ChatRoom.css";
 
 export function ChatRoom() {
   const localStorage = window.localStorage;
-  const [currentUser, setCurrentUser] = useState(
-    JSON.parse(localStorage.getItem("user"))
-  );
+  const [currentUser] = useState(JSON.parse(localStorage.getItem("user")));
 
   return (
     <div className="chat-room">
