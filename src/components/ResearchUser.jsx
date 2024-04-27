5578bd7ca4ecdb5dc777f672e53f582e224fb7ba
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2024-04-27 16:57:00 +0200

    Quand on clique dans l'input
    L'input se "décale" vers la droite.
    TODO : Mettre un boutton "fléché" vers la gauche (à la gauche de l'input), puis quand on clique dessus on restore le style. Quand on clique ailleurs avec la souris ça ne fait rien

--- src/components/ResearchUser.jsx
+++ src/components/ResearchUser.jsx
@@ -6,6 +6,7 @@ import { verifyToken } from "../utils/utils";
 
 export function ResearchUser() {
   const [searchTimeout, setSearchTimeout] = useState(null);
+  const [isMouseIn, setIsMouseOver] = useState(false);
 
   const navigate = useNavigate();
 
@@ -30,6 +31,14 @@ export function ResearchUser() {
         console.log(value);
       }, 300)
     );
+  }
+
+  function changeStyleOfInput() {
+    setIsMouseOver(true);
+  }
+
+  function restoreDefaultStyle() {
+    setIsMouseOver(false);
   }
 
   async function fetchUser(usernameToSearch) {
@@ -59,7 +68,12 @@ export function ResearchUser() {
 
   return (
     <div className="researchUser-container">
-      <svg className="icon" aria-hidden="true" viewBox="0 0 24 24">
+      <svg
+        className="icon"
+        aria-hidden="true"
+        viewBox="0 0 24 24"
+        style={{ marginLeft: isMouseIn ? "13%" : "initial" }}
+      >
         <g>
           <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
         </g>
@@ -69,6 +83,12 @@ export function ResearchUser() {
         className="user-research"
         placeholder="Rechercher un utilisateur..."
         onChange={handleChange}
+        // when my mouse is on the input, the margin-left is 13%
+        onFocus={changeStyleOfInput}
+        onBlur={restoreDefaultStyle}
+        style={{
+          marginLeft: isMouseIn ? "13%" : "initial",
+        }}
       />
       <Toaster />
     </div>
