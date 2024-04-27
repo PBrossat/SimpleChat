6700a6e97408e130aca898115e904ee6f6310371
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2024-04-28 00:39:17 +0200

    Ajout du non retour en arrière

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -7,13 +7,15 @@ export function ChatRoom() {
   const localStorage = window.localStorage;
   const [currentUser] = useState(JSON.parse(localStorage.getItem("user")));
 
+  // Refuse to go back to the previous page (work only twice)
+  window.history.pushState(null, "", window.location.href);
+
   return (
     <div className="chat-room">
       {/* barre latérale  */}
       {/* liste des discussions */}
       <Discussions />
       {/* chat */}
-      Bonjour : {currentUser.username}
     </div>
   );
 }
