4316c65636c16745b0527db06b138f019e83900a
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2024-04-28 00:38:42 +0200

    Ajout d'un tooltip pour le bouton de création de discussion

--- src/components/CreateDiscussion.jsx
+++ src/components/CreateDiscussion.jsx
@@ -1,4 +1,5 @@
 import "../style/CreateDiscussion.css";
+import Tooltip from "@mui/material/Tooltip";
 
 export function CreateDiscussion() {
   function handleClick() {
@@ -6,34 +7,36 @@ export function CreateDiscussion() {
   }
   return (
     <div className="create-discussion-div">
-      <button className="button-creation-disccusion" onClick={handleClick}>
-        <svg width="30px" height="30px" viewBox="0 0 24 24">
-          <g>
+      <Tooltip title="Créer une discussion" placement="right">
+        <button className="button-creation-disccusion" onClick={handleClick}>
+          <svg width="30px" height="30px" viewBox="0 0 24 24">
             <g>
               <g>
                 <g>
-                  <path
-                    d="M20,16v4a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V6A2,2,0,0,1,4,4H8"
-                    fill="none"
-                    stroke="#000000"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.704"
-                  ></path>
-                  <polygon
-                    fill="none"
-                    points="12.5 15.8 22 6.2 17.8 2 8.3 11.5 8 16 12.5 15.8"
-                    stroke="#000000"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.704"
-                  ></polygon>
+                  <g>
+                    <path
+                      d="M20,16v4a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V6A2,2,0,0,1,4,4H8"
+                      fill="none"
+                      stroke="#000000"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="1.704"
+                    ></path>
+                    <polygon
+                      fill="none"
+                      points="12.5 15.8 22 6.2 17.8 2 8.3 11.5 8 16 12.5 15.8"
+                      stroke="#000000"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="1.704"
+                    ></polygon>
+                  </g>
                 </g>
               </g>
             </g>
-          </g>
-        </svg>
-      </button>
+          </svg>
+        </button>
+      </Tooltip>
     </div>
   );
 }
