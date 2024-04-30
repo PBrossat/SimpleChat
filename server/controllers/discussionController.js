541b60cfd44ef8a0fbcb94e94af46da453eab49c
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2024-04-30 13:51:17 +0200

    Création de discussionController

---
+++ server/controllers/discussionController.js
@@ -0,0 +1,129 @@
+import sqlite3 from "sqlite3";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
+// Obtain the "pwd path" of the current file
+const __filename = fileURLToPath(import.meta.url);
+
+// Obtain the "pwd path" of the directory where the current file is located
+const __dirname = dirname(__filename);
+
+// Define the path to the database
+const DB_PATH = path.resolve(__dirname, "../DB_Simple_chat.db");
+
+export function addDiscussion(discussion) {
+  return new Promise((resolve, reject) => {
+    const db = new sqlite3.Database(DB_PATH);
+
+    // created_at is a field in the database that is automatically filled with the current date and time
+    const sql = `INSERT INTO Conversations (name, type, reator_id) VALUES (?, ?, ?)`;
+    const values = [discussion.name, discussion.type, discussion.creator_id];
+
+    db.run(sql, values, function (error) {
+      if (error) {
+        console.error("Error adding discussion to the database:", error);
+        reject(error);
+      } else {
+        resolve(this.lastID); // If there is no error, resolve the promise and send the discussion
+      }
+      db.close(); // Close the database connection
+    });
+  });
+}
+
+export function getActiveDiscussions(userId) {
+  return new Promise((resolve, reject) => {
+    const db = new sqlite3.Database(DB_PATH);
+
+    const sql = `
+        SELECT c.id, c.name, c.type, c.creator_id, c.created_at
+        FROM Conversations c
+        JOIN Participants p ON c.id = p.conversation_id
+        WHERE p.user_id = ?; 
+      `;
+    const values = [userId];
+
+    db.all(sql, values, (error, rows) => {
+      db.close(); // Close the database connection
+
+      if (error) {
+        console.error(
+          "Error getting active discussions from the database:",
+          error
+        );
+        reject(error);
+      } else {
+        const activeDiscussions = rows.map((row) => {
+          return {
+            id: row.id,
+            name: row.name,
+            type: row.type,
+            created_at: row.created_at,
+            creator_id: row.creator_id,
+          };
+        });
+        resolve(activeDiscussions); // If there is no error, resolve the promise and send the active discussions
+      }
+    });
+  });
+}
+
+// Function to get the participants of a conversation
+export function getParticipants(conversationId) {
+  return new Promise((resolve, reject) => {
+    const db = new sqlite3.Database(DB_PATH);
+
+    const sql = `
+        SELECT Users.id, Users.username, Users.email, Users.name, Users.surname
+        FROM Users
+        INNER JOIN Participants ON Users.id = Participants.user_id
+        WHERE Participants.conversation_id = ?
+      `;
+    const values = [conversationId];
+
+    db.all(sql, values, (error, rows) => {
+      db.close(); // Close the database connection
+
+      if (error) {
+        console.error(
+          "Erreur lors de la récupération des participants depuis la base de données :",
+          error
+        );
+        reject(error);
+      } else {
+        const participants = rows.map((row) => {
+          return {
+            id: row.id,
+            username: row.username,
+            email: row.email,
+            name: row.name,
+            surname: row.surname,
+          };
+        });
+        resolve(participants); // If there is no error, resolve the promise
+      }
+    });
+  });
+}
+
+export function addParticipantToDiscussion(participant, discussionId) {
+  return new Promise((resolve, reject) => {
+    const db = new sqlite3.Database(DB_PATH);
+
+    const sql = `INSERT INTO Participants (user_id, conversation_id) VALUES (?, ?)`;
+    const values = [participant.id, discussionId];
+
+    db.run(sql, values, function (error) {
+      if (error) {
+        console.error(
+          "Error adding participant to the discussion in the database:",
+          error
+        );
+        reject(error);
+      } else {
+        resolve(); // If there is no error, resolve the promise
+      }
+      db.close(); // Close the database connection
+    });
+  });
+}
---
+++ server/routes/createDiscussion.js
@@ -0,0 +1,50 @@
+import { Router } from "express";
+import { verifyToken } from "../utils/utils.js";
+import {
+  addDiscussion,
+  addParticipantToDiscussion,
+} from "../controllers/discussionController.js";
+
+const router = Router();
+
+router.post("/createDiscussion", async (req, res) => {
+  const token = req.headers.authorization.split(" ")[1];
+
+  let user = null;
+  user = await verifyToken(token); // Verify and decode the token
+
+  if (user.error) {
+    return res.status(401).send(user.error);
+  }
+
+  const body = req.body;
+  const creatorId = user.id; // the creator is the one who called the route
+  const nameDiscussion = body.nameDiscussion;
+  const participants = body.participants;
+  const typeDiscussion = participants.length <= 2 ? "0" : "1"; // 0 for private, 1 for group
+
+  const discussion = {
+    creator_id: creatorId,
+    name: nameDiscussion,
+    type: typeDiscussion,
+  };
+
+  try {
+    // Create the discussion
+    const createdDiscussionId = await addDiscussion(discussion);
+
+    // Associate participants with the discussion when the discussion is created
+    await Promise.all(
+      participants.map(async (participant) => {
+        await addParticipantToDiscussion(participant, createdDiscussionId);
+      })
+    );
+
+    res.status(200).send("Discussion created successfully");
+  } catch (error) {
+    console.error("Error creating discussion or adding participants:", error);
+    res.status(500).send("Error creating discussion or adding participants");
+  }
+});
+
+
