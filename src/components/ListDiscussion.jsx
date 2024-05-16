978b4de6b950bfc3431f9bbfb37ac297deb61b5a
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2024-05-16 19:29:58 +0200

    Minor change

--- src/components/ListDiscussion.jsx
+++ src/components/ListDiscussion.jsx
@@ -104,7 +104,7 @@ export function ListDiscussion() {
       clearTimeout(setTimeOut);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [navigate]);
 
   function displayListDiscussion() {
     const currentUserId = JSON.parse(localStorage.getItem("user")).id;
