01ef91b7491e40a15bda02f943075dddf21602f5
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2024-06-13 23:22:39 +0200

    maj create discussion component
    simplification du code, ajout d'une modal de validation, toujours un beug sur l'id non unique

--- src/components/CreateDiscussionInput.jsx
+++ src/components/CreateDiscussionInput.jsx
@@ -1,40 +1,39 @@
 import * as React from "react";
-import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
-import { useAutocomplete } from "@mui/base/useAutocomplete";
-import { X } from "lucide-react";
-import { Check } from "lucide-react";
+import Autocomplete from '@mui/material/Autocomplete';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
 import LinearProgress from "@mui/material/LinearProgress";
 import { verifyToken } from "../utils/utils";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
-import {
-  Root,
-  InputWrapper,
-  StyledTag,
-  Listbox,
-} from "../style/CreateDiscussionInput.styles";
-
-export function Tag(props) {
-  const { label, onDelete, ...other } = props;
-  return (
-    <div {...other}>
-      <span>{label}</span>
-      <X onClick={onDelete} />
-    </div>
-  );
-}
-
-Tag.propTypes = {
-  label: PropTypes.string.isRequired,
-  onDelete: PropTypes.func.isRequired,
-};
+import Button from '@mui/material/Button';
 
 export function CreateDiscussionInput() {
   const navigate = useNavigate();
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [selectedUsers, setSelectedUsers] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const currentUser = JSON.parse(localStorage.getItem("user"));
+
+
+  const styleModal = {
+    position: 'absolute',
+    top: '20%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '1px solid #000',
+    borderRadius: 2,
+    boxShadow: 24,
+    p: 4,
+  };
+  
   useEffect(() => {
     const fetchUser = async () => {
       try {
@@ -51,6 +50,11 @@ export function CreateDiscussionInput() {
         }
 
         const data = await req.json();
+        console.log(data);
+        // Sort the users by name
+        data.sort((a, b) => {
+          return a.name.localeCompare(b.name);
+        });
         setUsers(data);
       } catch (error) {
         toast.error("Nous n'avons pas pu récupérer les utilisateurs");
@@ -62,54 +66,76 @@ export function CreateDiscussionInput() {
     fetchUser();
   }, [navigate]);
 
-  const currentUser = JSON.parse(localStorage.getItem("user"));
-  const defaultValue = currentUser ? [currentUser] : []; // the default value is the current user
+  const handleAutocompleteChange = (event, value) => {
+    setSelectedUsers(value);
+  };
 
-  const {
-    getRootProps,
-    getInputProps,
-    getTagProps,
-    getListboxProps,
-    getOptionProps,
-    groupedOptions,
-    value,
-    focused,
-    setAnchorEl,
-  } = useAutocomplete({
-    id: "customized-hook-demo",
-    multiple: true,
-    options: users,
-    defaultValue: defaultValue,
-    getOptionLabel: (user) => user.name + " " + user.surname, // we can find by title and year
-  });
+  const handleClickCreate = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleConfirmCreateGroup = async () => {
+    // If the current user is not in the selected users, add it
+    if (!selectedUsers.includes(currentUser)) {
+      selectedUsers.push(currentUser);
+    }
+    console.log(selectedUsers);
+  };
 
   if (loading) {
     return <LinearProgress />;
   }
 
   return (
-    <Root>
-      <div {...getRootProps()}>
-        <InputWrapper ref={setAnchorEl} className={focused ? "focused" : ""}>
-          {value.map((user, index) => (
-            <StyledTag
-              label={user.name + " " + user.surname} // the tag contain the name and the surname of the user
-              {...getTagProps({ index })}
-            />
-          ))}
-          <input {...getInputProps()} />
-        </InputWrapper>
-      </div>
-      {groupedOptions.length > 0 ? (
-        <Listbox {...getListboxProps()}>
-          {groupedOptions.map((option, index) => (
-            <li {...getOptionProps({ option, index })}>
-              <span>{option.name + " " + option.surname}</span>
-              <Check />
-            </li>
-          ))}
-        </Listbox>
-      ) : null}
-    </Root>
+    <div>
+      <Autocomplete
+        multiple
+        id="tags-standard"
+        options={users}
+        onChange={handleAutocompleteChange}
+        defaultValue={currentUser}
+        getOptionLabel={(option) => option.name + " " + option.surname}
+        value={selectedUsers} 
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Votre groupe : "
+            placeholder="Ajoutez des utilisateurs"
+          />
+        )}
+        style={{ width: "99%", margin: "10px" }}
+      />
+      {selectedUsers.length > 0 && (
+        <Button variant="outlined" color="success" style={{marginLeft:"10px"}} onClick={handleClickCreate}>
+          Créer discussion
+        </Button>
+      )}
+      <Modal
+        open={isModalOpen}
+        onClose={handleCloseModal}
+        aria-labelledby="title"
+        aria-describedby="description"
+      >
+        <Box sx={styleModal}>
+          <Typography id="title" variant="h6" component="h2">
+            Confirmation
+          </Typography>
+          <Typography id="description" sx={{ mt: 2}}>
+            Êtes-vous sûr de vouloir créer un groupe avec les utilisateurs suivants : vous, 
+            {selectedUsers.map((user) => user.name + " " + user.surname).join(", ")} ?
+          </Typography>
+          <Button variant="outlined" color="error" style={{marginRight:"10px"}} onClick={handleCloseModal}>
+            Annuler 
+          </Button>
+          <Button variant="outlined" color="success"  onClick={handleConfirmCreateGroup}>
+            Confirmer
+          </Button>
+        </Box>
+      </Modal>
+    </div>
   );
 }
